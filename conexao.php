ad70d94dfee69ad9ce997afed6e5fdc6863e5211
Ricardo Soares <ricardo-72@github.com>
2023-01-02 17:03:34 -0300

    ajustes diversos

---
+++ conexao.php
@@ -0,0 +1,7 @@
+<?php
+$servidor = "127.0.0.1";
+$usuario = "root";
+$senha = "";
+$name = "bd_ebd";
+$conn = mysqli_connect($servidor, $usuario, $senha, $name);
+?>
---
+++ processa.php
@@ -0,0 +1,23 @@
+<?php   include_once "conexao.php"; ?>
+
+<?php
+$id = $_POST['us_id'];
+$nome = $_POST['us_nome'];
+$sobrenome = $_POST['us_sobrenome'];
+$celular = $_POST['us_celular'];
+$email = $_POST['us_email'];
+$cargo = $_POST['us_cargo'];
+$igreja = $_POST['us_igreja'];
+$senha = $_POST['us_senha'];
+
+$sql = "INSERT INTO tb_usuario (us_nome, us_sobrenome, us_celular, us_email, us_cargo, us_igreja, us_senha) value ('$id','$nome', '$sobrenome', '$celular', '$email' '$cargo', '$igreja', '$senha')";
+
+if (mysqli_query($conn, $sql)){
+
+    header("location: cadastro.php");
+
+} else{
+    echo "Erro na conexão" . $sql . "<br>" . mysqli_error($conn);
+    mysqli_close($conn);
+}
+?>
